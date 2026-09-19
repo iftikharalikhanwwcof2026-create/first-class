744f0a5c4edd1168f5fb9ef796f6b91bb71c59a2
Iftikhar Saadat <iftikhar.alikhan.wwcof.2026@gmail.com>
2026-09-19 17:10:17 +0500

    this is my first class

---
+++ script.js
@@ -0,0 +1,69 @@
+let web_dev ={
+hassan:{
+    fullname:"hasan iqbal",
+    age:90,
+
+    academic:[
+  {
+    name: "Saad",
+    age: 16,
+    gender: "Male",
+    marks: 85
+  },
+  {
+    name: "Zulfiqar",
+    age: 17,
+    gender: "Male",
+    marks: 78
+  },
+  {
+    name: "Areeba",
+    age: 16,
+    gender: "Female",
+    marks: 92
+  },
+  {
+    name: "Faheem",
+    age: 17,
+    gender: "Male",
+    marks: 88
+  },
+  {
+    name: "Zeeshan",
+    age: 16,
+    gender: "Male",
+    marks: 75
+  },
+  {
+    name: "Alveena",
+    age: 15,
+    gender: "Female",
+    marks: 95
+  },
+  {
+    name: "Musa",
+    age: 17,
+    gender: "Male",
+    marks: 81
+  },
+  {
+    name: "Nadeia",
+    age: 16,
+    gender: "Female",
+    marks: 89
+  },
+  {
+    name: "Hassan",
+    age: 15,
+    gender: "Male",
+    marks: 73
+  },
+  {
+    name: "Hina",
+    age: 17,
+    gender: "Female",
+    marks: 90},
+],
+}
+}
+console.log(web_dev.hassan.fullname)
